638971c6a85ff45f4ebc601a07d2c5e4dbd4b989
VyacheslavSamarin <slava.samarin.95@mail.ru>
2024-10-01 14:32:44 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "Pastebin-backend"
+//include("api-gateway")
+//include("auth")
+//include("generation")
+//include("paste")
+//include("notification")
+//include("user")
+//include("storage")
