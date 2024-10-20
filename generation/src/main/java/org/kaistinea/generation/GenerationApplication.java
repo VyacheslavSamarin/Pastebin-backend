44031e42cb0e9420ba1ad93ca6f207598064d698
vyacheslav <slava.samarin.95@gmail.com>
2024-10-20 16:03:53 +0300

    add resource server to services

--- generation/src/main/java/org/kaistinea/generation/GenerationApplication.java
+++ generation/src/main/java/org/kaistinea/generation/GenerationApplication.java
@@ -1,11 +1,13 @@
 package org.kaistinea.generation;
 
+import org.kaistinea.common.annotation.KaistineaResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@KaistineaResourceServer
 public class GenerationApplication {
     public static void main(String[] args) {
         SpringApplication.run(GenerationApplication.class, args);
--- paste/src/main/java/org/kaistinea/paste/PasteApplication.java
+++ paste/src/main/java/org/kaistinea/paste/PasteApplication.java
@@ -1,9 +1,11 @@
 package org.kaistinea.paste;
 
+import org.kaistinea.common.annotation.KaistineaResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@KaistineaResourceServer
 public class PasteApplication {
     public static void main(String[] args) {
         SpringApplication.run(PasteApplication.class, args);
