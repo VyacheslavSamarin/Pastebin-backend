a254608998a5ac4e7e5dd57f98a46b69e9a697a9
vyacheslav <slava.samarin.95@gmail.com>
2024-10-20 15:16:49 +0300

    add find first by available

--- generation/src/main/java/org/kaistinea/generation/repository/KeyRepository.java
+++ generation/src/main/java/org/kaistinea/generation/repository/KeyRepository.java
@@ -4,4 +4,5 @@ import org.kaistinea.generation.entity.KeyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeyRepository extends JpaRepository<KeyEntity, Long> {
+    KeyEntity findFirstByIsAvailableTrue();
 }
