489beaf7e4924734872c960b48b96d7ea12bec95
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:35:14 +0300

    init notification service

--- notification/src/main/java/org/kaistinea/notification/service/NotificationService.java
+++ notification/src/main/java/org/kaistinea/notification/service/NotificationService.java
@@ -1,4 +1,12 @@
 package org.kaistinea.notification.service;
 
+import org.kaistinea.notification.dto.NotificationDTO;
+import org.kaistinea.notification.dto.NotificationRequest;
+
+import java.util.List;
+
 public interface NotificationService {
+    List<NotificationDTO> getNotificationsByUserId(Long userId);
+
+    void sendNotification(NotificationRequest notificationRequest);
 }
