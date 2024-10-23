b26c4e1a6b1493aeac39a8d20855e910218afd9b
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:58:20 +0300

    rm

--- notification/src/main/java/org/kaistinea/notification/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package org.kaistinea.notification.service;
-
-import org.kaistinea.notification.dto.NotificationRequest;
-
-public interface EmailService {
-    void sendEmailNotification(NotificationRequest notification);
-}
--- notification/src/main/java/org/kaistinea/notification/service/PushService.java
+++
@@ -1,7 +0,0 @@
-package org.kaistinea.notification.service;
-
-import org.kaistinea.notification.dto.NotificationRequest;
-
-public interface PushService {
-    void sendPushNotification(NotificationRequest notification);
-}
