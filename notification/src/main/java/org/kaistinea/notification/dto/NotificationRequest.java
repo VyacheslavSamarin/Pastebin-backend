c9a6ebc3e26d5b00d6d675daca823045aae7d95f
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:35:45 +0300

    add fields

--- notification/src/main/java/org/kaistinea/notification/dto/NotificationRequest.java
+++ notification/src/main/java/org/kaistinea/notification/dto/NotificationRequest.java
@@ -1,4 +1,23 @@
 package org.kaistinea.notification.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+import org.kaistinea.common.enums.NotificationType;
+
+@Jacksonized
+@Builder(toBuilder = true)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationRequest {
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private NotificationType type;
+
+    private String content;
+
 }
