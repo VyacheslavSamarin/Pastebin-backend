632dc96f828ce456e5732f08a49d4e2f838a47b6
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:36:40 +0300

    add get all and send notification methods

--- notification/src/main/java/org/kaistinea/notification/controller/NotificationController.java
+++ notification/src/main/java/org/kaistinea/notification/controller/NotificationController.java
@@ -1,4 +1,36 @@
 package org.kaistinea.notification.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.kaistinea.notification.dto.NotificationDTO;
+import org.kaistinea.notification.dto.NotificationRequest;
+import org.kaistinea.notification.service.NotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/notifications")
 public class NotificationController {
+    private final NotificationService notificationService;
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{userId}"
+    )
+    public ResponseEntity<List<NotificationDTO>> getAllNotificationsByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok(notificationService.getNotificationsByUserId(userId));
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST
+    )
+    public ResponseEntity<Void> sendNotification(NotificationRequest notificationRequest) {
+        notificationService.sendNotification(notificationRequest);
+        return ResponseEntity.noContent().build();
+    }
 }
