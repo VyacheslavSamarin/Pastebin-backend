9e7c4f790718b650b137a761b81ae8fa861bfd24
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:37:23 +0300

    impl for services

---
+++ notification/src/main/java/org/kaistinea/notification/service/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package org.kaistinea.notification.service;
+
+import org.kaistinea.notification.dto.NotificationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void sendEmailNotification(NotificationRequest notification) {
+
+    }
+}
