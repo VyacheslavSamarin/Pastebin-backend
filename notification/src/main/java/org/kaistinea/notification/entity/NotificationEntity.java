a5ab088c55341ba06f3499f6ec98b2630866bd38
vyacheslav <slava.samarin.95@gmail.com>
2024-10-23 17:35:48 +0300

    add fields

--- notification/src/main/java/org/kaistinea/notification/entity/NotificationEntity.java
+++ notification/src/main/java/org/kaistinea/notification/entity/NotificationEntity.java
@@ -1,4 +1,29 @@
 package org.kaistinea.notification.entity;
 
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import lombok.*;
+import org.kaistinea.common.enums.NotificationType;
+
+import java.time.OffsetDateTime;
+
+@Table(name = "notification")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private NotificationType type;
+
+    private String content;
+
+    private OffsetDateTime sendAt = OffsetDateTime.now();
+
 }
