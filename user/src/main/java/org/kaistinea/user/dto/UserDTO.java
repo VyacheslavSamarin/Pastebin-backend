b36051c3b152ca06882f7d89d59018a285d89703
vyacheslav <slava.samarin.95@gmail.com>
2024-10-24 20:17:08 +0300

    feat: addition fields

--- user/src/main/java/org/kaistinea/user/dto/UserDTO.java
+++ user/src/main/java/org/kaistinea/user/dto/UserDTO.java
@@ -1,4 +1,43 @@
 package org.kaistinea.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.OffsetDateTime;
+
+@Jacksonized
+@Builder(toBuilder = true)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    @Email
+    private String email;
+
+    private String avatar;
+
+    private String bio;
+
+    @NotNull
+    private Boolean isPublic;
+
+    @NotNull
+    private OffsetDateTime creationDate;
+
+    @NotNull
+    private OffsetDateTime updatedDate;
+
+    @NotNull
+    private UserSettingsDTO userSettings;
+
 }
