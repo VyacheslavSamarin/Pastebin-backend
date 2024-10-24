baf4a555719dafc44fc937a0326dd23ffeb375a6
vyacheslav <slava.samarin.95@gmail.com>
2024-10-24 20:18:18 +0300

    feat: addition methods for user profile

--- user/src/main/java/org/kaistinea/user/service/UserService.java
+++ user/src/main/java/org/kaistinea/user/service/UserService.java
@@ -1,4 +1,19 @@
 package org.kaistinea.user.service;
 
+import org.kaistinea.user.dto.CreateUserRequest;
+import org.kaistinea.user.dto.UpdateUserRequest;
+import org.kaistinea.user.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    Long createUser(CreateUserRequest createUserRequest);
+
+    UserDTO getUserById(Long id);
+
+    List<UserDTO> getUsers();
+
+    void deleteUserById(Long id);
+
+    void updateUser(UpdateUserRequest updateUserRequest, Long id);
 }
