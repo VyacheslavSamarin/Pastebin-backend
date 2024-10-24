6e39c3bd10621629bb484bf9787ea5082a474093
vyacheslav <slava.samarin.95@gmail.com>
2024-10-24 20:17:38 +0300

    feat: addition mapper methods

--- user/src/main/java/org/kaistinea/user/mapper/UserMapper.java
+++ user/src/main/java/org/kaistinea/user/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package org.kaistinea.user.mapper;
 
+import org.kaistinea.user.dto.UserDTO;
+import org.kaistinea.user.entity.UserEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(
+        componentModel = "spring",
+        uses = UserSettingsMapper.class
+)
 public interface UserMapper {
+    UserEntity toEntity(UserDTO userDTO);
+
+    UserDTO toDto(UserEntity userEntity);
 }
