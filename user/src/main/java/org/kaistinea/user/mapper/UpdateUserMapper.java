c0e454adad906b1d8f47219394f9f6086e65be21
vyacheslav <slava.samarin.95@gmail.com>
2024-10-24 20:34:56 +0300

    refactor: addition settings for mapper

--- user/src/main/java/org/kaistinea/user/mapper/UpdateUserMapper.java
+++ user/src/main/java/org/kaistinea/user/mapper/UpdateUserMapper.java
@@ -2,11 +2,14 @@ package org.kaistinea.user.mapper;
 
 import org.kaistinea.user.dto.UpdateUserRequest;
 import org.kaistinea.user.entity.UserEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+
 )
 public interface UpdateUserMapper {
     void updateUser(@MappingTarget UserEntity userEntity, UpdateUserRequest updateUserRequest);
